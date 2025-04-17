655bd61f4b509a7aff31092651bec0ce28aa73ae
MsM <msmofworld@gmail.com>
2025-04-17 14:01:05 +0600

    Delete generate_m3u8.sh

--- generate_m3u8.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "#EXTM3U" > playlist.m3u8
-while IFS= read -r line; do
-  name=$(basename "$line" .mp4)
-  echo "#EXTINF:-1,$name" >> playlist.m3u8
-  echo "$line" >> playlist.m3u8
-done < mp4_links.txt
